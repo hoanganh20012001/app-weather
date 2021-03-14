046cd0957b9be3b1591580219ac8e64a12810a98
Anhnh <hoahoanganh20012001@gmail.com>
2021-03-14 16:23:59 +0700

    update code

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const app = {
         .then(data => this.displayWeather(data))
     },
     displayWeather(data) {
+        console.log(data)
         const { list } = data
         const currentWeather = list[0]
         const { name } = currentWeather
@@ -16,5 +17,43 @@ const app = {
         const { temp, humidity} = currentWeather.main
         const { speed } = currentWeather.wind
         console.log(name, icon, description, temp, humidity, speed)
+        $('.city').innerText = `Weather in ${name}`
+        $('.description').innerText = `${description}`
+        $('.temp').innerText = `${temp}°C`
+        $('.humidity').innerText = `Humidity: ${humidity}%`
+        $('.wind').innerText = `Wind speed: ${speed} km/h`
+        $('.icon').setAttribute('src', `
+            https://openweathermap.org/img/wn/${icon}.png
+        `)
+        document.body.style.backgroundImage = `
+            url('https://source.unsplash.com/1600x900/?${name}')
+        `
+        document.querySelector(".weather").classList.remove("loading")
+    },
+    searchCity() {
+        const inputCity = $('.search-bar')
+        inputCity.onchange = function(e) {
+            app.fecthWeahter(e.target.value)
+        }
+        inputCity.value = ''
+    },
+    handleEvents() {
+        const btnSearch = $('.search button')
+
+        btnSearch.addEventListener('click', function() {
+            app.searchCity()
+        })
+
+        btnSearch.addEventListener("keyup", function (event) {
+            console.log(event.key)
+            if (event.key === "Enter") {
+              app.searchCity()
+            }
+        })
+    },
+    run() {
+        this.handleEvents()
+        this.searchCity()
     }
 }
+app.run()
